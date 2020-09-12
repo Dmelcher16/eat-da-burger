5b517bd4e3ba3912c4b3e4ca11fc8ff2a84ea0d0
Dak <67485000+Dmelcher16@users.noreply.github.com>
2020-09-11 22:35:04 -0700

    server running

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Cheese Burger');
+INSERT INTO burgers (name) VALUES ('Bacon Burger');
+INSERT INTO burgers (name, devoured) VALUES ('Avo Burger', true);
+-- INSERT INTO cats (name, sleepy) VALUES ('Marilyn Manson', true);
+-- INSERT INTO cats (name, sleepy) VALUES ('Joe Biden', true);
+-- INSERT INTO cats (name) VALUES ('Owens');
